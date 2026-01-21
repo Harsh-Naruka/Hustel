1c0835ecf71e9cd673756df6b2783563a7c189c0
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-21 20:44:33 +0530

    a

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -13,17 +13,17 @@ int main() {
         }
         bool c=false;
         int count=0;
-        for(int i=0;i<n-2;i++){
-           if(a[i]<a[i+1])c=true;
+        for(int i=0;i<n-1;i++){
+           if(a[i]<=a[i+1])c=true;
            else c=false;
            int b=n-2;
-           if(a[b]<=a[b+1]){
+           if(a[b]<=a[b+1])
             a[b]=a[b]+1;
             a[b+1]=a[b+1]-1;
             count+=1;
            }
         }
-        if(c=false)cout<<0<<'\n';
+        if(c==false)cout<<0<<'\n';
         else{
            cout<<count<<'\n';
         } 

2c4b71f9a8db580de9252e501356faa6e307724d
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-22 17:45:13 +0530

    2110B

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -7,7 +7,9 @@ int main() {
     while(t--){
         string s;
         cin>>s;
+
         int p=0;
+        
         for(int i=0;i<s.size()-1;i++){
             if(s[i]=='('&&s[i+1]==')'){
                 p++;

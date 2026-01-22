6550dbd0c9994edb3a38b530c4ad1de3cbf74da9
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-22 17:42:59 +0530

    2110B

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,52 +1,20 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 
 int main() {
     int t;
-    cin >> t;
-
-    while (t--) {
-        int n;
-        cin >> n;
-
-        long long a[n];
-
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-
-        // Check if already not sorted
-        bool sorted = true;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (a[i] > a[i + 1]) {
-                sorted = false;
-                break;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int p=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]=='('&&s[i+1]==')'){
+                p++;
             }
         }
-
-        // If already not sorted
-        if (!sorted) {
-            cout << 0 << endl;
-            continue;
-        }
-
-        // Find minimum adjacent difference
-        long long minDiff = LLONG_MAX;
-
-        for (int i = 0; i < n - 1; i++) {
-            long long diff = a[i + 1] - a[i];
-            if (diff < minDiff) {
-                minDiff = diff;
-            }
-        }
-
-        // Minimum operations
-        long long answer = (minDiff / 2) + 1;
-
-        cout << answer << endl;
+        if(p>=2)cout<<"YES";
+        else cout<<"NO";
     }
-
     return 0;
 }

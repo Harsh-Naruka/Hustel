14cb7f74b94948da241942e0304ce162bf83ff52
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-25 20:28:55 +0530

    xx

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,19 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+
+    while (t--) {
         string s;
-        cin>>s;
-        int p=0,x=0;
-        for(char c:s){
-            if(c=='(')p++;
-            else x++;
+        cin >> s;
+
+        string target = "()()";
+        int j = 0;
+
+        for (char c : s) {
+            if (c == target[j])
+                j++;
+            if (j == 4)
+                break;
         }
-        if(p>=2&&x>=2)cout<<"YES"<<'\n';
-        else cout<<"NO"<<'\n';
+
+        if (j == 4)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
+
     return 0;
 }

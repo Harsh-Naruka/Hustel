0002e0533fc7a383d20b5e352e9e8ad181420cc7
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-25 20:32:07 +0530

    1087A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
     int t;
     cin>>t;
-    
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c)cout<<"+"<<'\n';
+        else cout<<"-"<<'\n';
+    }
     return 0;
 }

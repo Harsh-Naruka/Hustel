434afb206ff47734691c0fd1513dcaa3cdbd3969
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-25 20:30:05 +0530

    1087A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -2,18 +2,5 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-
-    while (t--) {
-        string s;
-        cin >> s;
-
-        string target = "()()";
-
-
     return 0;
 }

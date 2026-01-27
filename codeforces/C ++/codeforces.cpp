ccd75d7955f6c68afb7208cd6c1a26baccaa2518
Harsh-Naruka <hnaruka91@gmail.com>
2026-01-27 23:02:06 +0530

    1854A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,25 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false); 
-    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
-    cin>>t;
-    while(t--){
-        int n;
-        long long int x,y;
-        string s;
-        cin>>s;
-        int c4=0,c8=0;
-        for(char c:s){
-            if(c=='8')c8++;
-            else c4++;
-        }
-        long long dx=llbs(x);
-        long long dy=llbs(y);
-        if(dx+dy <= d4 + 2*c8 && max(dx,dy)<=d4+d8)cout<<"YES\n";
-        else cout<<"NO\n";
+    cin >> t;
+
+    while (t--) {
+        int n,k,x;
+        cin>>n>>k>>x;
     }
+
     return 0;
 }

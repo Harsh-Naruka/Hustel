b28e01bf31fc3da8b807656a47602d3a8c06d3c8
Harsh-Naruka <hnaruka91@gmail.com>
2026-02-23 23:59:07 +0530

    1988B

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,8 +1,29 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     int t;
-    cin>>t;
-    return 0;
+    cin >> t;
+    while(t--){
+        int n;
+        string s;
+        cin >> n >> s;
+        
+        int ones = 0, zero_blocks = 0;
+        bool in_zero = false;
+        
+        for(char c : s){
+            if(c == '1'){
+                ones++;
+                in_zero = false;
+            } else {
+                if(!in_zero){
+                    zero_blocks++;
+                    in_zero = true;
+                }
+            }
+        }
+        
+        cout << (ones > zero_blocks ? "Yes" : "No") << "\n";
+    }
 }

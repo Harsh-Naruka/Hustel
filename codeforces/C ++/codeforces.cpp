cf78611dfd38ea08df59c0d787dce9a6125ac4bd
Harsh-Naruka <hnaruka91@gmail.com>
2026-02-23 23:48:27 +0530

    .

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -4,22 +4,5 @@ using namespace std;
 int main() {
     int t;
     cin>>t;
-    while(t--){
-        long long int a,b,c,d;
-        cin>>a>>b>>c>>d;
-        int c;
-        if(a==c&&b==d)cout<<"0\n";
-        if(b>d)cout<<"-1\n";
-        else if(b<d){
-            a++;
-            c++
-            b++;
-        }
-        else if(b==d&&a<c){
-            a--;
-            c++;
-        }
-        cout<<"c"<<"\n";
-    }
     return 0;
 }

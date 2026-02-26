98e41b71f4cf45beacad5fbf006af2150dbd2ebe
Harsh-Naruka <hnaruka91@gmail.com>
2026-02-26 21:54:49 +0530

    2148C

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,29 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int t;
     cin >> t;
     while(t--){
-        int n;
-        string s;
-        cin >> n >> s;
+        long long n, m;
+        cin >> n >> m;
         
-        int ones = 0, zero_blocks = 0;
-        bool in_zero = false;
+        long long ans = 0;
+        long long curSide = 0;
+        long long curTime = 0;
         
-        for(char c : s){
-            if(c == '1'){
-                ones++;
-                in_zero = false;
-            } else {
-                if(!in_zero){
-                    zero_blocks++;
-                    in_zero = true;
-                }
-            }
+        for(int i = 0; i < n; i++){
+            long long a, b;
+            cin >> a >> b;
+            
+            long long slots = a - curTime;
+            long long needOdd = (b != curSide) ? 1 : 0;
+            
+            long long best = slots;
+            if(best % 2 != needOdd) best--;
+            
+            ans += best;
+            curSide = b;
+            curTime = a;
         }
         
-        cout << (ones > zero_blocks ? "Yes" : "No") << "\n";
+        // Last segment
+        ans += (m - curTime);
+        cout << ans << "\n";
     }
+    return 0;
 }

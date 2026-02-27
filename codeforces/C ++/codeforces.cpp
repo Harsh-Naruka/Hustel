c9c1ccad49ae223af66d37eaa513d2c34d4f4006
Harsh-Naruka <hnaruka91@gmail.com>
2026-02-27 20:09:02 +0530

    2042B

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,35 +1,33 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<set>
 using namespace std;
 
 int main(){
     int t;
     cin >> t;
     while(t--){
-        long long n, m;
-        cin >> n >> m;
-        
-        long long ans = 0;
-        long long curSide = 0;
-        long long curTime = 0;
-        
-        for(int i = 0; i < n; i++){
-            long long a, b;
-            cin >> a >> b;
-            
-            long long slots = a - curTime;
-            long long needOdd = (b != curSide) ? 1 : 0;
-            
-            long long best = slots;
-            if(best % 2 != needOdd) best--;
-            
-            ans += best;
-            curSide = b;
-            curTime = a;
+        int n;
+        int a[n];
+        int b[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            b[i]=a[i];
+        }
+        set<int> s(b,b+n);
+        if(s.size()==1)cout<<1<<'\n';
+        else{
+            if(n%2==0)count+=1;
+            sort(a,a+n);
+            for(int i=0;i<n-1;i++){
+                if(a[i]==a[i+1])count+=1;
+                else if(a[i]!=a[i+1]){
+                    count+=2;
+                }
+            }
         }
         
-        // Last segment
-        ans += (m - curTime);
-        cout << ans << "\n";
+        cout<<count<<'\n';
     }
     return 0;
 }

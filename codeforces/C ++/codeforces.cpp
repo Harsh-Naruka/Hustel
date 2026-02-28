0efd6ae7933b69f3bc940375e78773c686f1b31d
Harsh-Naruka <hnaruka91@gmail.com>
2026-02-28 18:47:09 +0530

    160A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -3,13 +3,20 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int count=0;
+    int count1=0 , count2=0;
     int coin[n];
     for(int i=0;i<n;i++){
         cin>>coin[i];
-    }
-    if(n==2)cout<<'2';
-    else{
-        
+        if(n%2==0){
+            if(i<(n/2)+1){
+                count1+=coin[i];
+            }
+            else{
+                count2+=coin[i];
+            }
+        }
+        else{
+            
+        }
     }
 }

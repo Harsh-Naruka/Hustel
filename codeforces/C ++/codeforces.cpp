c1305b57c57d00a433695bc0a33715ed6fd24095
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-01 22:20:15 +0530

    405A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
-    string s; cin>>s;
-    if( s.find("H")!=string::npos || s.find("Q")!=string::npos || s.find("9")!=string::npos){
-        cout<<"YES";
+    int n ,r; cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>r;
+        a[i]=r;
     }
-    else{
-        cout<<"NO";
+    sort(a,a+n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
     }
     return 0;
 }

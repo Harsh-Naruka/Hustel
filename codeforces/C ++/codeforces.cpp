25363d91b762844640b569432d8d695ca7d91ab1
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-01 21:46:39 +0530

    318A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,21 +1,13 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 int main(){
-    int n; cin>>n;
-    int a[n] ; int coin=0, sum=0 , crt=0;
-
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-        sum+=a[i];
+    long long n ,k; cin>>n>>k;
+    n=(n+1)/2;
+    if(k<=n){
+        cout<<(k*2)-1;
     }
-    sum/=2;
-    sort(a,a+n,greater<int>());
-    
-    for(int i=0;i<n;i++){
-        crt+=a[i];
-        coin++;
-        if(crt>sum)break;
+    else{
+        cout<<(k-n)*2;
     }
-    cout<<coin;
+    return 0;
 }

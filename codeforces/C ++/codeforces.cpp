566182da7e561f8012e48f9b4d9107d9bd94d589
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-02 16:28:10 +0530

    208A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -3,12 +3,10 @@ using namespace std;
 int main(){
     string s;
     cin>>s;
-    for(int i=0;i<s.size();i++){
-        if(s.find("WUB")){
-            cout<<s[i+1];
-        }
-        else{
-            cout<<s[i];
+    bool seen=false;
+    for(char c:s){
+        if(!seen && s.find("WUB")==string::npos){
+            cout<<c;
         }
     }
     return 0;

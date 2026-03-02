def85dfca45932d66e3a37faa0b64b33ae956e3c
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-02 19:00:05 +0530

    208A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
-int main(){
-    string s;
-    cin>>s;
-    bool seen=false;
-    for(char c:s){
-        if(!seen && s.find("WUB")==string::npos){
-            cout<<c;
-        }
+int main () {
+string s; cin>>s;
+int flag = 0;
+for (int i=0; i<s.size(); i++)
+    if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B') {
+        if (flag==1){ cout<<" ";
+        i += 2;}
+    else {cout<<s[i], flag=1;
     }
-    return 0;
+}
+return 0;
 }

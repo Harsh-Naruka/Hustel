959494197e3c81821ba26bb752a2465db53daeb0
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-02 16:25:46 +0530

    208A

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 int main(){
-    int n ,r; cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++){
-        cin>>r;
-        a[i]=r;
-    }
-    sort(a,a+n);
-    for(int i=0;i<n;i++){
-        cout<<a[i]<<" ";
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s.find("WUB")){
+            cout<<s[i+1];
+        }
+        else{
+            cout<<s[i];
+        }
     }
     return 0;
 }

3bd6a1788d07bf822ae2e631f1d810750970787c
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-05 23:26:20 +0530

    2094C

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main () {
-string s; cin>>s;
-int flag = 0;
-for (int i=0; i<s.size(); i++)
-    if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B') {
-        if (flag==1){ cout<<" ";
-        i += 2;}
-    else {cout<<s[i], flag=1;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<vector<int>> G(n+1, vector<int>(n+1));
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                cin >> G[i][j];
+        
+        for(int k = 2; k <= 2*n; k++){
+            // find valid i,j where i+j=k, 1<=i,j<=n
+            int i = max(1, k-n);
+            int j = k - i;
+            cout << G[i][j];
+            if(k < 2*n) cout << " ";
+        }
+        cout << "\n";
     }
-}
-return 0;
 }

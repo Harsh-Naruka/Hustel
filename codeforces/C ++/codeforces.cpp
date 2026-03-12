935e948895364c72db844a3817ee4f0294269e8b
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-12 11:46:10 +0530

    451A codeforces

--- codeforces/C ++/codeforces.cpp
+++ codeforces/C ++/codeforces.cpp
@@ -1,24 +1,7 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        vector<vector<int>> G(n+1, vector<int>(n+1));
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++)
-                cin >> G[i][j];
-        
-        for(int k = 2; k <= 2*n; k++){
-            // find valid i,j where i+j=k, 1<=i,j<=n
-            int i = max(1, k-n);
-            int j = k - i;
-            cout << G[i][j];
-            if(k < 2*n) cout << " ";
-        }
-        cout << "\n";
-    }
+	int n,m ; cin>>n>>m;
+	if(min(n,m)%2==0)cout<<"Malvika";
+	else cout<<"Akshat";
 }

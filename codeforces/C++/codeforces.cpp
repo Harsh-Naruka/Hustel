3ea63d12bcc565fddc89dcfbcaeb273274ac7067
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-30 03:31:36 +0530

    1475A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,14 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int n,m; cin>>n>>m;
-	int arr[n];
-	int c=0;
-	for(int i=0;i<n;i++){cin>>arr[i];}
-	sort(arr,arr+n);
-	for(int i=0;i<n;i++){
-		if(arr[i]>=0)break;
-		c=c+arr[i]
+	int t; cin>>t;
+	while(t--){
+		long 
 	}
-	cout<<abs(c);
 }

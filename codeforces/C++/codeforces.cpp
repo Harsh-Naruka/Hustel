c46f35d8bb4a391877e27a03ca3a5c85d2c91a7e
Harsh-Naruka <hnaruka91@gmail.com>
2026-03-31 23:15:00 +0530

    1457B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,9 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-	int t; cin>>t;
-	while(t--){
-		long n ; cin>>n;
-		
-	}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int y = n % 2020;          // count of 2021s
+        int x = (n - y) / 2020 - y; // count of 2020s
+        if (x >= 0 && 2020 * x + 2021 * y == n)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
 }

ac23fc71215b46e5144b9e6e3cd63f8f44e198ba
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-02 23:07:06 +0530

    460A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,23 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        long long n; cin>>n;
-        int a=0;
-        int b=0;
-        while(n%2==0){
-            a++;
-            n=n/2;
-        }
-        while(n%3==0){
-            b++;
-            n=n/3;
-        }
-        if(n!=1 || b<a)cout<<"-1\n";
-        else{
-            cout<<(b+(b-a))<<endl;
-        }
-        }
-    }
+    int n,m; cin>>n>>m;
+    int day=0;
+    
+

ac8f9b4387a7077549fb8ea58babd378bf1f36d2
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-03 21:43:11 +0530

    474A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    char c; cin>>c;
+    char c,i; cin>>c;
     string s="qwertyuiopasdfghjkl;zxcvbnm,./";
-    
+    string a;cin>>a;
+    while(cin>>i){
+        if(c=='R') cout<<s[s.find(i)-1];
+        else cout<<s[s.find(i)+1];
+    }
 }    

e04ccbb755330def36a2c879470168970d801ba5
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-03 21:48:02 +0530

    474A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -3,10 +3,7 @@ using namespace std;
 int main() {
     char c,i; cin>>c;
     string s="qwertyuiopasdfghjkl;zxcvbnm,./";
-    string a;cin>>a;
     while(cin>>i){
-        if(c=='R') cout<<s[s.find(i)-1];
-        else cout<<s[s.find(i)+1];
+        cout<<s[s.find(i)-(c=='R')+(c=='L')];
     }
 } 
-   

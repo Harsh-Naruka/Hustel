29b621e7611311e5b301605b016a1bcf8bc7279c
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-03 21:35:23 +0530

    474A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,11 +1,7 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n,m; cin>>n>>m;
-    int day=0;
-    for(int i=1;i<=n;i++){
-        if(i%m==0)n++;
-        day++;
-    }
-    cout<<day;
+    char c; cin>>c;
+    string s="qwertyuiopasdfghjkl;zxcvbnm,./";
+    
 }    

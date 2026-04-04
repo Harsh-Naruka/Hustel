7be80f1958a9c17bba7d7ec791be4b554429a8cf
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-04 21:12:00 +0530

    Q3

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int t; cin>>t;
+int main(){
+    int t;
+    cin >> t;
     while(t--){
         int n;
         cin >> n;
         for(int i = 1; i <= n; i++){
-            cout << i << " " << n+i << " " << 2*n+i;
+            cout << i << " " << n+i << " " << 2*n+i<<" ";
             if(i < n) cout << " ";
         }
         cout << "\n";
     }
-} 
-
+}

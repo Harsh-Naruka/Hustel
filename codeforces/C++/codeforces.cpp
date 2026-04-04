0d134b4fb66e1ea20302ab6a086bd682dd276c90
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-04 20:58:54 +0530

    q1

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main() {
     int t; cin>>t;
     while(t--){
-        int a; cin>>a;
-        cout<<a+1;
+        int a[7];
+        int mx=-68,sum=0;
+        for(int i=0;i<7;i++){
+            cin>>a[i];
+            mx=max(a[i],mx);
+            sum+=a[i];
+        }
+        cout<<-sum+(2*mx)<<'\n';
     }
 } 

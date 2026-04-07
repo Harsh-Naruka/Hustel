84445c23cb3b2a2cb11ea420132120f7bbe383bc
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-07 16:14:42 +0530

    149A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,13 +1,18 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    int n; cin>>n;
-    string s; cin>>s;
-    int a=0, b=0;
-    for(char c:s){
-        if(c=='0')a++;
-        else b++;
+    int k; cin>>k;
+    int a[11],mx=0;
+    for(int i=0;i<11;i++){
+        cin>>a[i];
     }
-    cout<<abs(a-b);
+    int count=0,sum=0;
+    sort(a,a+11);
+    for(int i=10;i>=0;i--){
+        if(sum<k){
+            sum+=a[i];
+            count++;
+        }
+    }
+    cout<<count;
 }

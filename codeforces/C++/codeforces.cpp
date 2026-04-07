16b2afc888e25f2dfcde1c081bb348b9d9878946
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-07 23:26:31 +0530

    149A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
     int k; cin>>k;
-    int a[11],mx=0;
-    for(int i=0;i<11;i++){
+    int a[12],mx=0;
+    for(int i=0;i<12;i++){
         cin>>a[i];
     }
     int count=0,sum=0;
-    sort(a,a+11);
-    for(int i=10;i>=0;i--){
+    sort(a,a+12);
+    for(int i=11;i>=0;i--){
         if(sum>=k)break;
             sum+=a[i];
             count++;

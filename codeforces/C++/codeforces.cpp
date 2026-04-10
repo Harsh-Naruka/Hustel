62bec66db993ae3c6f782127f46b739971f13d08
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-10 19:31:35 +0530

    1337B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,14 +1,18 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
  
 int main(){
 	int t; cin>>t;
 	while(t--){
-        int x,m,n; cin>>x<<m<<n;
-        int result=x-n;
-        if(result<=0) cout<<"YES\n",return 0;
-        for(int i=0;i<m;i++) result/=2;
-        if (result<=0)cout<<"YES\n";
+        int x,n,m; cin>>x>>n>>m;
+        if(x<=m){x=x-(m*10); cout<<x<<endl;}
+        else{
+        for(int i=0;i<n;i++){
+            x=(x/2)+10;
+            cout<<x<<endl;
+            }x=x-(m*10);
+        }
+        if (x<=0)cout<<"YES\n";
         else cout<<"NO\n";
 	}
 }

bd9511282f01a87c4d6aee35df96ca9d38ea7b1f
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-10 18:44:31 +0530

    1337B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -3,27 +3,7 @@ using namespace std;
  
 int main(){
 	int t; cin>>t;
-	
 	while(t--){
-	    
-		int a, b, c; cin>>a>>b>>c;
- 
-        //ye actual hai
-		int A = b - (c-b); 
-		int B = a + (c-a)/2;
-		int C = b + (b-a);
-		
-		//a mein dikkat
-		if(A!=0 && A>=a && A%a==0) cout<<"YES\n";
-		
-		//b mein dikkat
-		else if(B && B>=b && B%b==0 && (c-a)%2==0) cout<<"YES\n";
- 
-		//c mein dikkat
-		else if(C && C>=c && C%c==0) cout<<"YES\n";
- 
-		//kuch nhi ho skta
-		else cout<<"NO\n";
-		
+	
 	}
 }

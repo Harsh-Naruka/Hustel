b2da885aa9c8c3418fc95cab792ab076a5709bc9
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-10 19:37:48 +0530

    1337B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -5,13 +5,13 @@ int main(){
 	int t; cin>>t;
 	while(t--){
         int x,n,m; cin>>x>>n>>m;
-        if(x<=m) x=x-(m*10);
+        if(x<=m+n) x=x-(m*10);
         else{
         for(int i=0;i<n;i++){
             x=(x/2)+10;
             }x=x-(m*10);
         }
-        if (x<=m+n)cout<<"YES\n";
+        if (x<=0)cout<<"YES\n";
         else cout<<"NO\n";
 	}
 }

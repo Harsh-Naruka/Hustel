68a367b7501226012af434951b0b3709042f9c87
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-10 19:14:33 +0530

    1337B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -6,7 +6,7 @@ int main(){
 	while(t--){
         int x,m,n; cin>>x<<m<<n;
         int result=x-n;
-        if(result<=0) cout<<"YES\n",return 0;;
+        if(result<=0) cout<<"YES\n",return 0;
         for(int i=0;i<m;i++) result/=2;
         if (result<=0)cout<<"YES\n";
         else cout<<"NO\n";

c615b4ac391480373d45c7dcd15b536d06e29afa
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-10 18:00:54 +0530

    1642B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,13 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
-    int a,mx=0; cin>>a;
+    int a,mc=0; cin>>a;
     string s,ss,twogram; cin>>s;
     map<string ,int> smp;
 
     for(int i=0;i<a-1;i++){
-        ss=s[i]; ss+=s[i+1]; smp[ss]++;
-        if(smp[ss]>mc) mc=smp[ss] ,  togram=ss;
+        ss=s[i], ss+=s[i+1]; 
+        smp[ss]++;//AB:1//
+        if(smp[ss]>mc) mc=smp[ss] , twogram=ss;
     }
     cout<<twogram;
 }

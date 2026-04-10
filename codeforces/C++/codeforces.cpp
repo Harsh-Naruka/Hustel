d828be0beeb94cea83c948bc7459250579508ac1
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-10 19:13:14 +0530

    1337B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(){
 	int t; cin>>t;
 	while(t--){
-	
+        int x,m,n; cin>>x<<m<<n;
+        int result=x-n;
+        if(result<=0) cout<<"YES\n",return 0;;
+        for(int i=0;i<m;i++) result/=2;
+        
 	}
 }

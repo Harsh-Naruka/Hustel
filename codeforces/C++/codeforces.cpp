a533f4029a33b2ebad7be197bcc85489b9bab24b
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-11 17:53:57 +0530

    320A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string n; cin>>n;
-    bool flag=false;
-    for(int i=0;i<n.size()-2;i++){
-        if(n[i]=='1' || (n[i]=='1' && n[i+1]=='4') || (n[i]==1 && n[i+1]=='4' && n[i+2]=='4')) flag=true;
-        else {
-            flag=false;
+    string s; cin>>s;
+    for(int i=0;i<s.size();){
+        if(s[i]!='1' && s[i]!='4'){ cout<<"NO" ; return 0;}
+        else if(s[i]=='1'&&s[i+1]=='4'&&s[i+2]=='4') i+=3;
+        else if(s[i]=='1'&&s[i+1]=='4') i+=2;
+        else if(s[i]=='1')i++;
+        else{
+            cout<<"NO"; return 0;
         }
     }
-    if(flag==true) cout<<"YES";
-    else cout<<"NO";
+    cout<<"YES";
 }

8e5df062dfc2efe98481fd46a8dea8a0b9831f18
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-11 17:44:18 +0530

    320A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,8 +1,14 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
     string n; cin>>n;
-    for(int i=0;i<n.size();i++){
-        
+    bool flag=false;
+    for(int i=0;i<n.size()-2;i++){
+        if(n[i]=='1' || (n[i]=='1' && n[i+1]=='4') || (n[i]==1 && n[i+1]=='4' && n[i+2]=='4')) flag=true;
+        else {
+            flag=false;
+        }
     }
+    if(flag==true) cout<<"YES";
+    else cout<<"NO";
 }

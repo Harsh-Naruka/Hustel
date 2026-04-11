ac6d1ca88a06e84af1fea200a0e4ea95e743150d
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-11 17:33:32 +0530

    320A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,16 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
 int main(){
-    int t; cin>>t;
-    while(t--){
-        int x,n,m; cin>>x>>n>>m;
-        while(n--){
-            int temp = (x/2)+10;
-            if(x<=temp) break;
-            x = temp;
-        }
-        if(x<=m*10) cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
+    string n; cin>>n;
+    for(int i=0;i<n.size();i++){
+        
     }
 }

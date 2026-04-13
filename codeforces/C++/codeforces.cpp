7e0b19191b23bb69f035b604d52e15745e76f142
Your Name <your-email@gmail.com>
2026-04-13 10:02:18 +0530

    115A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string s; cin>>s;
-    for(int i=0;i<s.size();){
-        if(s[i]!='1' && s[i]!='4'){ cout<<"NO" ; return 0;}
-        else if(s[i]=='1'&&s[i+1]=='4'&&s[i+2]=='4') i+=3;
-        else if(s[i]=='1'&&s[i+1]=='4') i+=2;
-        else if(s[i]=='1')i++;
-        else{
-            cout<<"NO"; return 0;
-        }
-    }
-    cout<<"YES";
+	int n, a[2000], temp, ans=0, cnt; 
+	cin>>n;
+	for(int i=1; i<=n; i++){
+		cin>>a[i];
+	}
+	for(int i=1; i<=n; i++){
+		temp = a[i], cnt=1;
+		while(temp!=-1) cnt++, temp = a[temp];
+		ans = max(ans, cnt);
+	}
+	cout<<ans;
 }

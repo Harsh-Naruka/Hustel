e892786370faddef9a1a9238e45f3612dec5237a
sourav641 <sourav641@gmail.com>
2026-04-20 22:38:27 +0530

    1339A

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main(){
 	int t; cin>>t;
 	while(t--){
-		string s; cin>>s;
-		int a[2] = {};
-		for(char c : s) a[c-'0']++; 
-		cout<<(min(a[0],a[1])%2?"DA":"NET")<<endl;
+		int n; cin>>n;
+		cout<<n<<endl;
 	}
 }

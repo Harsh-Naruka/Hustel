49df1565761548c1a23652649fe1ffd774cf9387
Harsh-Naruka <hnaruka91@gmail.com>
2026-04-29 20:57:42 +0530

    1535B

--- codeforces/C++/codeforces.cpp
+++ codeforces/C++/codeforces.cpp
@@ -1,12 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
 int main(){
 	int t; cin>>t;
-	while(t--){
-		int n; cin>>n;
-		int a[n];
-		for(int i=0;i<n;i++){
-			cin>>a[i];
+	while(t--){	
+		int n, ans=0; cin>>n;
+		vector<int> a(n);
+		for(int &x: a) cin>>x;
+ 
+		//sorting based on even/odd
+		sort(a.begin(), a.end(), [](int x, int y){return x%2 < y%2;});
+ 
+		//cnt no. of good index pairs
+		for(int i=0; i<n; i++){
+			for(int j=i+1; j<n; j++){
+				ans += __gcd(a[i],a[j]*2)>1;
+			}
 		}
+		
+		cout<<ans<<endl;
 	}
 }
